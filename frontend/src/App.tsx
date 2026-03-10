76047ad86507a180a7aad04df69ef8ef39082513
Nicolas Ovidian Winsnes Marti <nicolas@Nicolass-MacBook-Pro.local>
2026-03-10 13:02:32 +0100

    feat(frontend): all data fetched from api displayed too user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+const GamesAPI = "http://localhost:3000/"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // fetching data from api
+  useEffect(() => {
+    fetch(GamesAPI + "games")
+      .then(res => res.json())
+      .then(json => setData(json))
+      .catch(err => console.error(err));
+  }, []); // empty array = runs once on mount
+
+
+ return <div>{data ? JSON.stringify(data) : "Loading..."}</div>;
 }
 
-export default App
+export default App;
