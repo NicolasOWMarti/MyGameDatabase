2740dd567c7c617abdd9a93453aa81823291f318
Nicolas Ovidian Winsnes Marti <nicolas@Nicolass-MacBook-Pro.local>
2026-03-25 11:04:26 +0100

    feat(app.js): made app.post function

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,9 @@ const port = 3000
 const database = require("./dbconnector.js")
 const cors =require('cors')
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+
 console.log("hi.")
 app.get('/games', async (req, res) => {
     console.log("hello")
@@ -15,7 +17,12 @@ app.get('/games', async (req, res) => {
     res.send( queryResult)
 })
 
-//app.post
+app.post('/newgame', async (req, res) => {
+  let forwardedData = req.body;
+  let queryResult = await database.query(`INSERT INTO Games (Title, Genre, AgeRestriction, SinglePlayer, MultiPlayer, ReleaseDate, Developer, Publisher) VALUES ('${forwardedData.Title}', '${forwardedData.Genre}', '${forwardedData.AgeRestriction}', '${forwardedData.SinglePlayer}', '${forwardedData.MultiPlayer}', '${forwardedData.ReleaseDate}', '${forwardedData.Developer}', '${forwardedData.Publisher}');`)
+  console.log(forwardedData);
+  res.send("hello");
+})
 
 // /users
 
