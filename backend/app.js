2eef3dedb965dbdb881d05783edd9c24fd4a3e51
Nicolas Ovidian Winsnes Marti <nicolas@Nicolass-MacBook-Pro.local>
2026-05-13 10:10:16 +0200

    feat(backend): made '/newgame' post function

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,9 @@ const database = require("./dbconnector.js")
 const cors =require('cors')
 
 app.use(cors())
+app.use(express.json())
 console.log("hi.")
+
 app.get('/games', async (req, res) => {
     console.log("hello")
     let queryResult = await database.query("SELECT * FROM games;")
@@ -15,7 +17,12 @@ app.get('/games', async (req, res) => {
     res.send( queryResult)
 })
 
-//app.post
+app.post('/newgame', async(req, res) => {
+  let forwaredData = req.body;
+  let queryResult = await database.query(`INSERT INTO games(Title, Genre, AgeRestriction, SinglePlayer, MultiPlayer, ReleaseDate, Developer, Publisher) VALUES ('${forwaredData.title}', '${forwaredData.genre}', '${forwaredData.AgeRestriction}', '${forwaredData.SinglePlayer}', '${forwaredData.MultiPlayer}', '${forwaredData.ReleaseDate}', '${forwaredData.Developer}', '${forwaredData.Publisher}');`)
+  console.log(queryResult)
+  res.send("hi, it worked")
+})
 
 // /users
 
