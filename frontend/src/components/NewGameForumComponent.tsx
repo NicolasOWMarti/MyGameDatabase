7dd72fdf47ecf784b21bb410bdc9bbd0bccfb680
Nicolas Ovidian Winsnes Marti <nicolas@Nicolass-MacBook-Pro.local>
2026-06-12 12:23:11 +0200

    feat: clicking the button now posts the testdata

--- frontend/src/components/NewGameForumComponent.tsx
+++ frontend/src/components/NewGameForumComponent.tsx
@@ -1,6 +1,30 @@
+const API = "http://localhost:3000/"
+
+async function PostHandler() {
+    
+    const testData = {
+        Title: "Blablabla",
+        Genre: "bLablabla",
+        AgeRestriction: "13",
+        SinglePlayer: "1",
+        MultiPlayer: "0",
+        ReleaseDate: "2022-12-14",
+        Developer: "blAblabla",
+        Publisher: "blaBlabla"
+    }
+
+    // posts test data
+    const result = await fetch(API + "newgame", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json; charset=utf-8'},
+        body: JSON.stringify(testData)
+    })
+    
+    console.log(result)
+}
 
 function NewGameForumComponent() {
-    return <button>hello</button>
+    return <button onClick={PostHandler}>hello</button> // runs PostHandler() when clicked
 };
 
 export default NewGameForumComponent;
